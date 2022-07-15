beba1c60e120fcab40582d1600bcd088967e6407
Prathamesh Kumbhar <prathameshkumbhar291113@gmail.com>
2022-07-16 01:30:11 +0530

    [New Screen]: Made the new Screen to display the detail news in NewsDetailActivity.java and created the UI for the same activity.

--- app/src/main/java/com/example/tothepointnews/NewsDetailActivity.java
+++ app/src/main/java/com/example/tothepointnews/NewsDetailActivity.java
@@ -1,11 +1,22 @@
 package com.example.tothepointnews;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import com.squareup.picasso.Picasso;
 
 public class NewsDetailActivity extends AppCompatActivity {
     String title,desc,content,imageURL,url;
+    private TextView newsTitle, newsDesc, newsContent;
+    private ImageView newsDetImg;
+    private AppCompatButton newsDetBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +26,24 @@ public class NewsDetailActivity extends AppCompatActivity {
         desc= getIntent().getStringExtra("desc");
         imageURL = getIntent().getStringExtra("image");
         url = getIntent().getStringExtra("url");
+        newsTitle = findViewById(R.id.NewsDetailTxt);
+        newsDesc = findViewById(R.id.NewsDetailDesc);
+        newsContent = findViewById(R.id.NewsDetailContent);
+        newsDetImg = findViewById(R.id.NewsDetailImg);
+        newsDetBtn = findViewById(R.id.NewsDetailReadNewsBtn);
+        newsTitle.setText(title);
+        newsDesc.setText(desc);
+        newsContent.setText(content);
+        Picasso.get().load(imageURL).into(newsDetImg);
+        newsDetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
 
 
     }
